49f4f1d3854a0636aeeea9884552a3efe5a7910b
RifatMahmudno-1 <rifatmahmudpc@gmail.com>
2025-07-10 02:20:59 +0600

    Added checks for KSU and SuSFS. Added description modifying functionality to show status.

--- customize.sh
+++ customize.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+SUSFS_BIN="/data/adb/ksu/bin/ksu_susfs"
+
+if [ "$KSU" != true ] ; then
+	abort '[!] This module requires KernelSU (Next)'
+fi
+
+if [ ! -f "$SUSFS_BIN" ] ; then
+    abort '[!] This module requires SuSFS'
+fi
+
 mkdir -p "$MODPATH/system/etc"
 cat "/system/etc/hosts" > "$MODPATH/system/etc/hosts"
 touch "$MODPATH/skip_mount"
--- service.sh
+++ service.sh
@@ -4,8 +4,27 @@ SUSFS_BIN="/data/adb/ksu/bin/ksu_susfs"
 target_hostsfile="$MODDIR/system/etc/hosts"
 system_hostfile="/system/etc/hosts"
 
-# Bind mount the custom hosts file
-mount --bind "$target_hostsfile" "$system_hostfile"
+update_description() {
+	sed -i "s|^description=.*|description=$(printf '%s' "$1")|" "$MODDIR/module.prop"
+}
 
-# Register with KernelSU SUSFS for persistent remount
-"$SUSFS_BIN" add_try_umount "$system_hostfile" 1
+if [ "$KSU" != true ] ; then
+    update_description 'KernelSU (Next) is required ❌'
+    exit 1
+elif [ ! -f "$SUSFS_BIN" ] ; then
+    update_description 'SuSFS is required ❌'
+    exit 1
+else
+    mount --bind "$target_hostsfile" "$system_hostfile"
+    "$SUSFS_BIN" add_try_umount "$system_hostfile" 1
+fi
+
+until [ "$(getprop sys.boot_completed)" = "1" ]; do
+    sleep 1
+done
+
+if [ -w "$system_hostfile" ]; then
+    update_description 'Module is working ✅'
+else
+    update_description 'Module not working ❌'
+fi
